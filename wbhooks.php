6f4477f848b6160d7716929b76966af172451824
m8wallis <marcwallis85@gmail.com>
2016-11-16 16:33:29 -0800

    Update wbhooks.php

--- wbhooks.php
+++ wbhooks.php
@@ -6,8 +6,6 @@ $verify_token = $_REQUEST['hub_verify_token'];
 if ($verify_token === 'abc1234') {
     echo $challenge;
 }
-else{
-    echo "Error, wrong validation token";
-}
+$input = json_decode(file_get_contents('php:://input'), true);
 
 ?>
