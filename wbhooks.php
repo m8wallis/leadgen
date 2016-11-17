83484bfd994e691d16c5ad2583a83b2d54d8999e
m8wallis <marcwallis85@gmail.com>
2016-11-17 10:17:12 -0800

    Update wbhooks.php

--- wbhooks.php
+++ wbhooks.php
@@ -6,11 +6,5 @@ $verify_token = $_REQUEST['hub_verify_token'];
 if ($verify_token === 'abc12345') {
     echo $challenge;
 }
-$input = json_decode(file_get_contents('php:://input'), true);
-
-use FacebookAds\Object\Lead;
-
-$form = new Lead(<LEAD_ID>);
-$form->read();
 
 ?>
