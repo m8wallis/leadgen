edd7020f674dd4ba9365247234b687f25f4a6d01
m8wallis <marcwallis85@gmail.com>
2016-11-18 13:40:28 -0800

    Update wbhooks.php

--- wbhooks.php
+++ wbhooks.php
@@ -20,7 +20,8 @@ error_log(print_r($input, true));
 
 preg_match("/[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})/i", $input, $email);
 
-echo hello;
-echo $email;
+foreach($email['data'] as $email) {
+    echo $email['type'], '<br>';
+}
 
 ?>
