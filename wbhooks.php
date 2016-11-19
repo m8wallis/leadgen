a0f5f8bb9e9f49d0e51cfd5c9b8bfa08bb9e623d
m8wallis <marcwallis85@gmail.com>
2016-11-18 18:56:58 -0800

    Update wbhooks.php

--- wbhooks.php
+++ wbhooks.php
@@ -42,6 +42,9 @@ $user_access_token = 'EAATeansT7awBABOrV8ZAbUZBkrLvReWJqmSJ8rAZAZC2qLlV1fNpbpqJe
 //Get the lead info
 $lead = getLead($leadgen_id,$user_access_token);//get lead info
 
+$fbfullname = $lead["entry"][0]["changes"][0]["value"]["full_name"];
+$fbemail = $lead["entry"][0]["changes"][0]["value"]["email"];
+
 //foreach($lead as $attr=>$val) {
 //    error_log(print_r($attr, true));
 //    error_log(print_r($val, true));
@@ -50,6 +53,8 @@ $lead = getLead($leadgen_id,$user_access_token);//get lead info
 //Create an email with the lead info
 $mail="<html><body><h2>New lead</h2><blockquote>";
 $mail.="Lead id: ".$leadgen_id."<br>";
+$mail.="Email: ".$fbemail."<br>";
+$mail.="Name: ".$fbfullname."<br>";
 foreach($lead as $attr=>$val) {
     $mail.=$attr.": ".$val."<br>";
 }
