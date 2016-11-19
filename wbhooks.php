a1a5c251cec0687fdf5d81306b47f7765e39f8ee
m8wallis <marcwallis85@gmail.com>
2016-11-18 20:00:14 -0800

    Update wbhooks.php

--- wbhooks.php
+++ wbhooks.php
@@ -26,9 +26,6 @@ function getLead($leadgen_id,$user_access_token) {
     //work with the lead data
     $leaddata = json_decode($output);
     
-    //print it to show i'm not insane
-    error_log(print_r($leaddata, true));
-    
     $lead = [];
     for($i=0;$i<count($leaddata->field_data);$i++) {
         $lead[$leaddata->field_data[$i]->name]=$leaddata->field_data[$i]->values[0];
@@ -44,39 +41,17 @@ foreach($lead as $attr=>$val) {
 $input = json_decode(file_get_contents('php://input'),true);
 $leadgen_id = $input["entry"][0]["changes"][0]["value"]["leadgen_id"];
 
-//declare variables for later output
-$fbfullname = $leaddata["field_data"][0]["values"][0];
-$fbemail = $leaddata["field_data"][0]["email"][0];
-
 //Token - you must generate this in the FB API Explorer - tip: exchange it to a long-lived (valid 60 days) token
 $user_access_token = 'EAATeansT7awBALJqOiA0L4M5BzO4cW8xawHak7f2FRd3ZB7jwWEklci9gsDVPiRCMOa83sZAWYXcaZCcppb2UoVF2CZBMiXAiLcpdgQKsL0yjOFfk1gaEeUBeKygZA1mXtjA9l2HWTbxwMNVOGzvJ';
 
 //Get the lead info
 $lead = getLead($leadgen_id,$user_access_token);//get lead info
 
-
-//foreach($lead as $attr=>$val) {
-//    error_log(print_r($attr, true));
-//    error_log(print_r($val, true));
-
-
-//Create an email with the lead info
-$mail="<html><body><h2>New lead</h2><blockquote>";
-$mail.="Lead id: ".$leadgen_id."<br>";
-$mail.="Email: ".$fbemail."<br>";
-$mail.="Name: ".$fbfullname."<br>";
-$mail.="Lead dump: ".$lead."<br>";
-foreach($lead as $attr=>$val) {
-    $mail.=$attr.": ".$val."<br>";
-}
-$mail.="</blockquote></body></html>";
-error_log(print_r($mail, true));
-
+//extract the data from facebook to new variables $email and $full_name
 extract($lead, EXTR_SKIP);
+
+//Print the variables to the error log to show it's working
 error_log(print_r($email, true));
 error_log(print_r($full_name, true));
-
-$string=implode(",",$lead);
-error_log(print_r($string, true));
 
 ?>
