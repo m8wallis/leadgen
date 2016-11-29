a14f52b4401c1155aca3e0605d7e0252f3de2645
m8wallis <marcwallis85@gmail.com>
2016-11-29 12:49:18 -0800

    Update wbhooks.php

--- wbhooks.php
+++ wbhooks.php
@@ -5,6 +5,7 @@ $fb_verify_token = 'abc12345'; //put in your facebook verify_token here
 $fb_user_access_token = 'EAATeansT7awBALJqOiA0L4M5BzO4cW8xawHak7f2FRd3ZB7jwWEklci9gsDVPiRCMOa83sZAWYXcaZCcppb2UoVF2CZBMiXAiLcpdgQKsL0yjOFfk1gaEeUBeKygZA1mXtjA9l2HWTbxwMNVOGzvJ'; //put in your facebook user access token here - you must generate this in the FB API Explorer - tip: exchange it to a long-lived (valid 60 days) token 
 $wc_api_realm = 'skullcandy'; //no need to change this, it's already populated with your whatcounts API realm
 $wc_api_password = 'etylinel4322'; //no need to change this unless you change your api password for whatcounts
+$wc_list_id = '115'; //here you need to put in the list ID for the whatcounts list you want the subscribers added to.  In this case, "115" is the ID of the "SKDYUS - TestAPI" list.
 //End SkullCandy customization section
     
 //Begin code to be left alone -------->
@@ -119,7 +120,7 @@ $subscriber
 $subscriber
     ->setOverrideConfirmation(false);
 $subscriber
-    ->setListId(115);
+    ->setListId($wc_list_id);
 
 $subscribers = $whatcounts->subscribe($subscriber);
 ?>
