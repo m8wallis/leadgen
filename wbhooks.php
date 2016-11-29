198b7d2c89301085b6b9a7f4cf6f794aa6f82f3b
m8wallis <marcwallis85@gmail.com>
2016-11-29 12:41:25 -0800

    Update wbhooks.php

--- wbhooks.php
+++ wbhooks.php
@@ -1,10 +1,10 @@
 <?php
 //Start SkullCandy customization section
 //This section is the first customized section.  The next starts at line 65, the final at line 95.
-$fb_verify_token = 'abc12345' //put in your facebook verify_token here
-$fb_user_access_token = 'EAATeansT7awBALJqOiA0L4M5BzO4cW8xawHak7f2FRd3ZB7jwWEklci9gsDVPiRCMOa83sZAWYXcaZCcppb2UoVF2CZBMiXAiLcpdgQKsL0yjOFfk1gaEeUBeKygZA1mXtjA9l2HWTbxwMNVOGzvJ' //put in your facebook user access token here - you must generate this in the FB API Explorer - tip: exchange it to a long-lived (valid 60 days) token 
-$wc_api_realm = 'skullcandy' //no need to change this, it's already populated with your whatcounts API realm
-$wc_api_password = 'etylinel4322' //no need to change this unless you change your api password for whatcounts
+$fb_verify_token = 'abc12345'; //put in your facebook verify_token here
+$fb_user_access_token = 'EAATeansT7awBALJqOiA0L4M5BzO4cW8xawHak7f2FRd3ZB7jwWEklci9gsDVPiRCMOa83sZAWYXcaZCcppb2UoVF2CZBMiXAiLcpdgQKsL0yjOFfk1gaEeUBeKygZA1mXtjA9l2HWTbxwMNVOGzvJ'; //put in your facebook user access token here - you must generate this in the FB API Explorer - tip: exchange it to a long-lived (valid 60 days) token 
+$wc_api_realm = 'skullcandy'; //no need to change this, it's already populated with your whatcounts API realm
+$wc_api_password = 'etylinel4322'; //no need to change this unless you change your api password for whatcounts
 //End SkullCandy customization section
     
 //Begin code to be left alone -------->
